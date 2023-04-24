b6d23240e5758437cb0985731ac3f6e594281147
DeadlyneCode <114301388+DeadlyneCode@users.noreply.github.com>
2023-04-24 14:23:33 +0200

    Initial commit

---
+++ data/notes/hey.hx
@@ -0,0 +1,25 @@
+-function onPlayerHit(noteData) {
+    switch(noteData) {
+        case 0:
+            playBFsAnim("hey", true);
+        case 1:
+            playBFsAnim("hey", true);
+        case 2:
+            playBFsAnim("hey", true);
+        case 3:
+            playBFsAnim("hey", true);
+    }
+
+}
+function onDadHit(noteData) {
+    switch(noteData) {
+        case 0:
+            playDadsAnim("hey", true);
+        case 1:
+            playDadsAnim("hey", true);
+        case 2:
+            playDadsAnim("hey", true);
+        case 3:
+            playDadsAnim("hey", true);
+    }
+}
