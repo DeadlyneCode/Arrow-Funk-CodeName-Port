48d1c31f9b8d539a728a4580e41535514ec14ed7
playeah2 <playeah50@gmail.com>
2023-04-25 16:30:17 +0200

    test

--- data/states/StoryMenuState.hx
+++
@@ -1,8 +0,0 @@
-function create(){
-    bg = new FlxSprite(0, 0).loadGraphic(Paths.image("menus/storymenu/storybg"));
-    bg.antialiasing = true;
-}
-function postCreate(){
-    add(bg);
-    StoryMenuState.instance.insert(StoryMenuState.instance.members.indexOf(weekBG)-1,bg);
-}
