355eb979ad9b39e02686e497e6e81e3bcd9e0c17
DeadlyneCode <114301388+DeadlyneCode@users.noreply.github.com>
2023-04-26 10:17:18 +0200

    oof

---
+++ data/states/StoryMenuState.hx
@@ -0,0 +1,10 @@
+package data.states;
+
+function create(){
+    bg = new FlxSprite(0, 0).loadGraphic(Paths.image("menus/storymenu/storybg"));
+    bg.antialiasing = true;
+}
+function postCreate(){
+    add(bg);
+    StoryMenuState.instance.insert(StoryMenuState.instance.members.indexOf(weekBG)-1,bg);
+}
