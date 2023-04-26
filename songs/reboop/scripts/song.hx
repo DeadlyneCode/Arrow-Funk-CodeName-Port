2b0e01278c330e2f8ce7283a39051dcff524e638
playeah2 <114688848+playeah2@users.noreply.github.com>
2023-04-26 17:16:42 +0200

    Update song.hx

--- songs/reboop/scripts/song.hx
+++ songs/reboop/scripts/song.hx
@@ -1,16 +1,17 @@
 function postCreate(){
-    song = new FlxSprite(-800, -150).loadGraphic(Paths.image('song/song-Reboop'));
-    song.antialiasing = true;
-    song.scale.set(0.7, 0.7);
-	add(song);
-    song.cameras = [camHUD];
+    song_name = new FlxSprite(-800, -150).loadGraphic(Paths.image('song/song-Tutorial'));
+    song_name.antialiasing = true;
+    song_name.scale.set(0.7, 0.7);
+	add(song_name);
+    song_name.cameras = [camHUD];
 }
 
 function stepHit(curStep:Int){
 	switch(curStep) {
 		case 1:
-		tween = FlxTween.tween(song, { x:-200 }, 0.5);
-			
-		case 15:
-		tween = FlxTween.tween(song, { x:-800 }, 0.5);
+		tween = FlxTween.tween(song_name, { x:-200 }, 3, { type: FlxTween.PINGPONG, ease: FlxEase.smoothStepInOut});
+
+                case 34:
+                tween.cancel();
+                song_name.destroy();
 }}
